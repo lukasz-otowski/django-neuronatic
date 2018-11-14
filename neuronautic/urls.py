99ea44d15272583ffa1f5e321f146fcd8ce0b1f7
Łukasz Otowski <lukaszotowski@interia.pl>
2018-11-14 23:17:53 +0100

    Redirecting the homepage

--- neuronautic/urls.py
+++ neuronautic/urls.py
@@ -21,13 +21,14 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from neuron import views as article_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^neuron/',include('neuron.urls')),
     url(r'^about/$',views.about),
-    url(r'^$',views.homepage),
+    url(r'^$', article_views.article_list, name="home"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
